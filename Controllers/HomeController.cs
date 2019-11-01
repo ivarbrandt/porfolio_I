8394f54731490d704920c6c23a0d55f499f8034f
Ivar Brandt <ibrandt2@gmail.com>
2019-11-01 11:17:43 -0600

    updated 11/1/2019

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,21 @@ namespace portfolio_I
         //Requests
         //localhost:5000
         [HttpGet("")]
-        public string Index()
+        public ViewResult Index()
         {
-            return "This is my Index!";
+            return View();
         }
 
         //localhost:5000/hello
         [HttpGet("projects")]
-        public string Projects()
+        public ViewResult Projects()
         {
-            return "These are my projects";
+            return View();
         }
         [HttpGet("contact")]
-        public string Contact()
+        public ViewResult Contact()
         {
-            return "This is my Contact!";
+            return View();
         }
     }
 }
